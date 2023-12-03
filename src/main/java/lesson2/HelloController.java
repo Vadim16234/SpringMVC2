603dfc7f6f081046f435ba326ffc018698d19d13
Vadik <gloom16@mail.ru>
2023-12-03 20:09:09 +0300

    initial commit

---
+++ src/main/java/lesson2/HelloController.java
@@ -0,0 +1,13 @@
+package lesson2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world!!!";
+    }
+}
